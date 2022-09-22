c748d8aecd45cc209a8e449534595a16a2bf2359
Тим <mirocnicencotimofej2008@gmail.com>
2022-09-22 20:03:57 +0300

    Added exception handler in main.py

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ def divider(a, b):
     return a / b
 
 
-data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8: 4}
+data = {10: 2, 2: 5, "123": 4, 18: 0, 8: 4}  # we can't use dict as a name of dict element
 for key in data:
-    res = divider(key, data[key])
-    result.append(res)
+    try:
+        res = divider(key, data[key])
+        result.append(res)
+    except Exception as ex:
+        ex_formated = str(type(ex)).split("'")[1]
+        print(f"Caught {ex_formated}: element {key}")
+
 print(result)
